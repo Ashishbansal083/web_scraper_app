d3b945799156766c645ec1ebea82614d7b3200b7
Ashish <bansalgashi083@gmail.com>
2025-03-15 23:42:53 +0530

    minor changes

--- src/app.js
+++ src/app.js
@@ -10,6 +10,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 // Routes
 app.use("/uploads", express.static(path.join(__dirname,"services", "uploads")));
 app.use("/", companyRoutes);
