d5f970b9d3ce563e4ddfa0e8d8bec43b4925c24e
Ashish <bansalgashi083@gmail.com>
2025-03-17 22:05:19 +0530

    minor changes

--- src/app.js
+++ src/app.js
@@ -1,23 +1,23 @@
-const express = require("express");
-const cors = require("cors");
-const companyRoutes = require("./routes/companyRoutes");
-const path = require("path");
+// const express = require("express");
+// const cors = require("cors");
+// const companyRoutes = require("./routes/companyRoutes");
+// const path = require("path");
 
 
-const app = express();
+// const app = express();
 
 // Middleware
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
 
 // Routes
-app.use("/uploads", express.static(path.join(__dirname,"services", "uploads")));
-app.use("/", companyRoutes);
+// app.use("/uploads", express.static(path.join(__dirname,"services", "uploads")));
+// app.use("/", companyRoutes);
 
-module.exports = app;
+// module.exports = app;
